f6b0bb50b59720a48d51500e0134eb13537f4b4a
Peter Senna Tschudin <peter.senna@gmail.com>
2018-04-02 19:18:10 +0200

    iwegarde does not run as root, so it can't use port 443
    Change it back to use port 5000 as it does not run as root. Also updates
    the start.sh script to fix some minor issues.
    
    Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@iwe.io>

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source venv/bin/activate
 flask db upgrade
-exec gunicorn --certfile fullchain.pem --keyfile privkey.pem -b :443 --access-logfile - --error-logfile - iwegarde:app
+exec gunicorn --certfile fullchain.pem --keyfile privkey.pem -b :5000 --access-logfile - --error-logfile - iwegarde:app
--- start.sh
+++ start.sh
@@ -2,29 +2,30 @@
 
 DATABASE_PASSWORD=V96vBX5apwvO8s3eu
 
+#	--mount type=bind,src=/home/peter/mysql-iwegarde,dst=/var/lib/mysql # For newer dockers...
 docker run \
 	--name mysql-iwegarde \
 	-d --rm \
-	-v /home/peter/mysql-iwegarde:/var/lib/mysql
-#	--mount type=bind,src=/home/peter/mysql-iwegarde,dst=/var/lib/mysql # For newer dockers...
+	-v /home/peter/mysql-iwegarde:/var/lib/mysql \
 	-e MYSQL_RANDOM_ROOT_PASSWORD=yes \
 	-e MYSQL_DATABASE=iwegarde \
 	-e MYSQL_USER=iwegarde \
 	-e MYSQL_PASSWORD=$DATABASE_PASSWORD \
 	mysql/mysql-server:5.7
 
-sleep 15
+sleep 5
 
+#	-it --entrypoint /bin/sh \
 docker run \
-	-d -rm \
+	-d --rm \
 	--name iwegarde-server \
-	-p 443:443 \
+	-p 443:5000 \
 	-e SECRET_KEY=4iCyF4zG3bxwueOFP32u \
 	-e MAIL_SERVER=smtp.googlemail.com \
 	-e MAIL_PORT=587 \
 	-e MAIL_USE_TLS=true \
 	-e MAIL_USERNAME=peter.senna.bot \
 	-e MAIL_PASSWORD=5qtn1MGljhAYvq \
-	--link mysql:dbserver \
+	--link mysql-iwegarde:dbserver \
 	-e DATABASE_URL=mysql+pymysql://iwegarde:$DATABASE_PASSWORD@dbserver/iwegarde \
 	iwegarde:latest
