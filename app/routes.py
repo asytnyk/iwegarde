5c0e82ab1c91dd75c31a59e1e9dc1e087724dce6
Peter Senna Tschudin <peter.senna@gmail.com>
2018-04-02 19:29:57 +0200

    remove remaining follower functionality
    There is no point in following other users at the moment. Remove acces
    to that information. Also remove the explore link.
    
    Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@iwe.io>

--- app/routes.py
+++ app/routes.py
@@ -163,6 +163,8 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
+    if current_user != User.query.filter_by(username=peter):
+        return redirect(url_for('index')
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
             page, app.config['POSTS_PER_PAGE'], False)
