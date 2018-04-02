dc721e4ac81c554f837f68a5e416683b8363c03c
Peter Senna Tschudin <peter.senna@gmail.com>
2018-04-02 18:36:32 +0200

    Added ssl keys from letsencrypt
    Added the keys and updated configuration so to enable ssl support.
    The certificate will expire on 2018-07-01, instructions on how to
    upgrade are on the readme.sslkeys file.
    
    Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@iwe.io>

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+DATABASE_PASSWORD=V96vBX5apwvO8s3eu
+
+docker run \
+	--name mysql-iwegarde \
+	-d --rm \
+	-v /home/peter/mysql-iwegarde:/var/lib/mysql
+#	--mount type=bind,src=/home/peter/mysql-iwegarde,dst=/var/lib/mysql # For newer dockers...
+	-e MYSQL_RANDOM_ROOT_PASSWORD=yes \
+	-e MYSQL_DATABASE=iwegarde \
+	-e MYSQL_USER=iwegarde \
+	-e MYSQL_PASSWORD=$DATABASE_PASSWORD \
+	mysql/mysql-server:5.7
+
+docker run \
+	-d -rm \
+	--name iwegarde-server \
+	-p 443:443 \
+	-e SECRET_KEY=4iCyF4zG3bxwueOFP32u \
+	-e MAIL_SERVER=smtp.googlemail.com \
+	-e MAIL_PORT=587 \
+	-e MAIL_USE_TLS=true \
+	-e MAIL_USERNAME=peter.senna.bot \
+	-e MAIL_PASSWORD=5qtn1MGljhAYvq \
+	--link mysql:dbserver \
+	-e DATABASE_URL=mysql+pymysql://iwegarde:$DATABASE_PASSWORD@dbserver/iwegarde \
+	iwegarde:latest
