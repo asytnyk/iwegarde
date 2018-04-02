edc6e9a654421df6cc27f8f1713f90535390f752
Peter Senna Tschudin <peter.senna@gmail.com>
2018-04-02 20:15:01 +0200

    boot.sh: Move sleep from start script to inside container
    f5c869394e6197acb6579b40f5465b689132e407 removed sleep 5 from start
    script, but I forgot to git add boot.sh. This commit fixes the problem.
    
    Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@iwe.io>

--- boot.sh
+++ boot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sleep 5
 source venv/bin/activate
 flask db upgrade
 exec gunicorn --certfile fullchain.pem --keyfile privkey.pem -b :5000 --access-logfile - --error-logfile - iwegarde:app
