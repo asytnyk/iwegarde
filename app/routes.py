asytnyk/iwegarde

commit 966a2353bcb1df8fb13167162e583558660f6b41

routes.py: Add error logs to console
When adding nginx in front of the gunicorn server soming is wrong and
request_activation_pin fails. I tried to remove nginx to see if the
problem vanishes and I confirm it does. This patch adds log messages to
help find out what is wrong.

Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
Signed-off-by: Peter Senna Tschudin <peter.senna@iwe.io>

app/routes.py
@@ -298,26 +298,32 @@ def create_it_all(user, facter_json, session):
 @app.route('/request_activation_pin', methods=['GET', 'POST'])
 def request_activation_pin():
     if current_user.is_authenticated:
+        app.logger.error('authenticated user trying to get an activation pin: {}'.format(current_user.username))
         return render_template('404.html'), 404
 
     installation_key = request.headers.get('installation_key')
     if not installation_key:
+        app.logger.error('missing installation key on header')
         return render_template('404.html'), 404
 
     user = User.verify_installation_key_token(installation_key)
     if not user:
+        app.logger.error('Installation key verification failed')
         return render_template('404.html'), 404
 
     try:
         facter_json = request.get_json()
     except:
+        app.logger.error('could find facter json on the http request')
         return render_template('404.html'), 404
 
     try:
         json.dumps(facter_json)
     except:
+        app.logger.error('could not parse facter json')
         return render_template('404.html'), 404
     if not facter_json:
+        app.logger.error('facter json is empty')
         return render_template('404.html'), 404
 
     #TODO: Add mac address
@@ -334,6 +340,7 @@ def request_activation_pin():
                 ).first()
     except:
         error = {'error': 'The data we got is not right. Are you using the latest version of the installer?'}
+        app.logger.error(error['error'])
         return Response(json.dumps(error), mimetype='application/json')
 
     if facts:
@@ -343,6 +350,7 @@ def request_activation_pin():
                         url_for('server', uuid=server.uuid, _external=True))}
         else:
             error = {'error': 'Something is wrong. If this server was activated in the past, deactivate it before continuing.'}
+        app.logger.error(error['error'])
         return Response(json.dumps(error), mimetype='application/json')
 
     activation, facts, server = create_it_all(user, facter_json, db.session)

commit 41287cf66b355d31242c9300535ae444742da0b9

Change serveriry level from error to info
Commit 966a2353bcb1df8fb13167162e583558660f6b41 introduced log messages
with error severity which result in each message triggering an email to
my account. This patch changes the severity to info, to stop me from
spamming myself.

Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
Signed-off-by: Peter Senna Tschudin <peter.senna@iwe.io>

app/routes.py
@@ -298,32 +298,32 @@ def create_it_all(user, facter_json, session):
 @app.route('/request_activation_pin', methods=['GET', 'POST'])
 def request_activation_pin():
     if current_user.is_authenticated:
-        app.logger.error('authenticated user trying to get an activation pin: {}'.format(current_user.username))
+        app.logger.info('authenticated user trying to get an activation pin: {}'.format(current_user.username))
         return render_template('404.html'), 404
 
     installation_key = request.headers.get('installation_key')
     if not installation_key:
-        app.logger.error('missing installation key on header')
+        app.logger.info('missing installation key on header')
         return render_template('404.html'), 404
 
     user = User.verify_installation_key_token(installation_key)
     if not user:
-        app.logger.error('Installation key verification failed')
+        app.logger.info('Installation key verification failed')
         return render_template('404.html'), 404
 
     try:
         facter_json = request.get_json()
     except:
-        app.logger.error('could find facter json on the http request')
+        app.logger.info('could find facter json on the http request')
         return render_template('404.html'), 404
 
     try:
         json.dumps(facter_json)
     except:
-        app.logger.error('could not parse facter json')
+        app.logger.info('could not parse facter json')
         return render_template('404.html'), 404
     if not facter_json:
-        app.logger.error('facter json is empty')
+        app.logger.info('facter json is empty')
         return render_template('404.html'), 404
 
     #TODO: Add mac address
@@ -340,7 +340,7 @@ def request_activation_pin():
                 ).first()
     except:
         error = {'error': 'The data we got is not right. Are you using the latest version of the installer?'}
-        app.logger.error(error['error'])
+        app.logger.info(error['error'])
         return Response(json.dumps(error), mimetype='application/json')
 
     if facts:
@@ -350,7 +350,7 @@ def request_activation_pin():
                         url_for('server', uuid=server.uuid, _external=True))}
         else:
             error = {'error': 'Something is wrong. If this server was activated in the past, deactivate it before continuing.'}
-        app.logger.error(error['error'])
+        app.logger.info(error['error'])
         return Response(json.dumps(error), mimetype='application/json')
 
     activation, facts, server = create_it_all(user, facter_json, db.session)