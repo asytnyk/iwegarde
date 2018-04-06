a7e0ac089a205c27fb51b0efd83525ade8c4fc90
Peter Senna Tschudin <peter.senna@gmail.com>
2018-04-06 11:12:33 +0200

    comment out file handler for logger
    Inside docker containers file handlers are not very useful, or at least
    complicated. Comment the code for now.
    
    Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@iwe.io>

--- app/__init__.py
+++ app/__init__.py
@@ -39,14 +39,15 @@ if not app.debug:
 
         app.logger.addHandler(mail_handler)
 
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
+#    if not os.path.exists('logs'):
+#        os.mkdir('logs')
+#    file_handler = RotatingFileHandler('logs/iWeFrontend.log', maxBytes=1048576, backupCount=10)
+#    file_handler.setFormatter(logging.Formatter(
+#        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+#    file_handler.setLevel(logging.INFO)
+#    app.logger.addHandler(file_handler)
 
-    file_handler = RotatingFileHandler('logs/iWeFrontend.log', maxBytes=1048576, backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+
     app.logger.setLevel(logging.INFO)
     app.logger.info('iWe Frontend startup')
 
