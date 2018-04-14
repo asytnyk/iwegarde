39c97ef0e7a0f8950155d9dde3c61075c8419350
Peter Senna Tschudin <peter.senna@gmail.com>
2018-04-14 13:47:17 +0200

    local-start.sh: Start containers on development env
    A subset of start.sh that start services that are needed for running
    iwegard on development environment. It also show instructions on how to
    start phpmyadmin.
    
    Signed-off-by: Peter Senna Tschudin <peter.senna@gmail.com>
    Signed-off-by: Peter Senna Tschudin <peter.senna@iwe.io>

---
+++ docker/local-start.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+DATABASE_PASSWORD=V96vBX5apwvO8s3eu
+PKI_PASSWORD=dt7ltvQew97XJt4uQF
+
+docker run \
+	--name mysql-iwegarde \
+	-d \
+	--restart unless-stopped \
+	-v /mnt/stateful_partition/iwe/mysql-iwegarde:/var/lib/mysql \
+	-p 3306:3306 \
+	-e MYSQL_RANDOM_ROOT_PASSWORD=yes \
+	-e MYSQL_DATABASE=iwegarde \
+	-e MYSQL_USER=iwegarde \
+	-e MYSQL_PASSWORD=$DATABASE_PASSWORD \
+	mysql/mysql-server:5.7
+
+docker run \
+	-d \
+	--restart unless-stopped \
+	--name iwe_pki \
+	-v /mnt/stateful_partition/iwe/pki/pki:/home/iwepki/easy-rsa3/pki \
+	-p 4000:5000 \
+	-e SECRET_KEY=$PKI_PASSWORD \
+	iwe_pki:latest
+
+echo mount /mnt/stateful_partition/iwe/client_vpn_config/ $HOME/iwe_client_vpn_config/ ...
+sudo mount -o bind /mnt/stateful_partition/iwe/client_vpn_config/ $HOME/iwe_client_vpn_config/
+
+echo "export DATABASE_URL=mysql+pymysql://iwegarde:$DATABASE_PASSWORD@localhost/iwegarde"
+echo "export PKI_SECRET=$PKI_PASSWORD"
+
+echo "docker run --name myadmin -d --link mysql-iwegarde:db -p 8080:80 phpmyadmin/phpmyadmin"
+echo "Then use iwegarde / $DATABASE_PASSWORD on the web interface"
